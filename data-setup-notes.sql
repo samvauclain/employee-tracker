30eca6b66f2ce8ee78d651de9881038ae7e77483
Sam Place <samvauclain@gmail.com>
2021-12-19 22:51:43 -0800

    db notes update

--- data-setup-notes.sql
+++ data-setup-notes.sql
@@ -35,9 +35,11 @@ CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER NOT NULL
 );
 
 
 DESCRIBE departments;
 DESCRIBE role;
+DESCRIBE employee;
