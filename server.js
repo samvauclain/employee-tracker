140d27545ca77fa104ba9fe981ca0f4f8998a6bb
Sam Place <samvauclain@gmail.com>
2021-12-21 22:03:29 -0800

    connected to db

---
+++ server.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql2');
+
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'mUu8>a8E-,E>;:7Q',
+      database: 'employee_tracker'
+    },
+    console.log('Connected to the employee tracker database.')
+);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
