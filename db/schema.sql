d9f727c936ffa2e231e7611eb59fe7b99946f4e7
Sam Place <samvauclain@gmail.com>
2021-12-25 12:19:28 -0800

    foreign key error

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,24 @@
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+-- DROP TABLE IF EXISTS employee;
+
 CREATE TABLE departments (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INTEGER NOT NULL
+  department_id INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL,
-  manager_id INTEGER NOT NULL
-);
+-- CREATE TABLE employee (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   first_name VARCHAR(30) NOT NULL,
+--   last_name VARCHAR(30) NOT NULL,
+--   role_id INTEGER,
+--   manager_id INTEGER
+-- );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,14 +4,15 @@ VALUES
   ('Marketing'),
   ('Development');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
-  ('Developer', 100000, 0),
+  ('Sales Person', 70000, 0),
   ('Marketing Manager', 80000, 1),
-  ('Marketing Manager', 70000, 1);
+  ('Developer', 100000, 2),
+  ('CEO', 280000, NULL);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES
-  ('Andy', 'Samberg', 0, 0),
-  ('Bill', 'Hader', 1, 1),
-  ('Amy', 'Poehler', 2, 2);
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id)
+-- VALUES
+--   ('Andy', 'Samberg', 0, 0),
+--   ('Bill', 'Hader', 1, 1),
+--   ('Amy', 'Poehler', 2, 2);
